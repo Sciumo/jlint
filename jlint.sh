814491197938cd377e68bb3b8d9aa498c2bf650a
C. Artho <c.artho@aist.go.jp>
2001-02-27 01:09:02 +0000

    Removed absolute path to jlint (doh!)

--- jlint.sh
+++ jlint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find . -name '*.class' | xargs /data/mthesis/jlint/jlint -not_overridden \
+find . -name '*.class' | xargs jlint -not_overridden \
 -redundant -weak_cmp -bounds -zero_operand -string_cmp -shadow_local | \
 grep -v 'Field .class\$' | grep -v 'can be .*ed from different threads' | \
 grep -v 'Method.*Runnable.*synch'
